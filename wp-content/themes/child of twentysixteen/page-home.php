0fd647fa0fc674ed004a71cea756f0a65e3a0460
Victoria Jin <victoriahjin@gmail.com>
2018-02-13 19:01:59 -0500

    Initial commit of class wordpress site

---
+++ wp-content/themes/child of twentysixteen/page-home.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The template modified for displaying content on new default home page called page-home.php
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<?php
+		// Start the loop.
+		while ( have_posts() ) : the_post();
+
+			// Include the page content template.
+			//using a new template to load content on home page called no-title-page.php
+			get_template_part( 'no-title', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) {
+				comments_template();
+			}
+
+			// End of the loop.
+		endwhile;
+		?>
+
+	</main><!-- .site-main -->
+
+	<?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
